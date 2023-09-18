dcf7182d3426fc965a99d8d152c1d50219295fb1
Seoyoung Oh <ohseoyoung5@gmail.com>
2023-09-18 18:14:32 +0900

    lab5

--- testapp/App.js
+++ testapp/App.js
@@ -4,22 +4,24 @@ import { StyleSheet, Text, View, Image, TextInput, ScrollView, Button} from 'rea
 
 var text_st = {fontSize: 30, backgroundColor: 'lightgray', padding:10, margin:10};
 
-var input_st = {fontSize:30, borderWidth:1, padding: 10, margin: 10};
+// var input_st = {fontSize:30, borderWidth:1, padding: 10, margin: 10};
 
 export default function App() {
 
-  const [ greet, setGreet] = useState('Hi');
-  const [ name, setName ] = useState('Name');
+//  const [ greet, setGreet] = useState('Hi');
+//  const [ name, setName ] = useState('Name');
+  const [ val, setval ] = useState(0);
 
   return (
     <ScrollView contentContainerStyle={{paddingTop: 30}}>
-      <Text style={text_st}>{greet}, {name}</Text>
-      <TextInput style={input_st} onChangeText={ setName } />
+      <Text style={text_st}>{val}</Text>
 
-      <View style={{margin:10, flexDirection: "row-reverse"}}>
-        <Button title="NICE" onPress={ function() { setGreet('Nice to meet you') }} />
-        <View style={{width:10}}></View>
-        <Button title="Hello" onPress={ function() { setGreet('Hello') }} />
+      <View style={{ margin:20 }}>
+        <Button title="Count Up" 
+              onPress={ function() { setval(val+1) }} />
+        <View style={{ height:10 }}></View>
+        <Button title="Count Down" 
+              onPress={ function() { setval(val-1) }} />
       </View>
       
     </ScrollView>
