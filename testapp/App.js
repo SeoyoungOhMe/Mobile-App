51d5014b9b0c88d2de78813bb2570f5c151dbdc5
Seoyoung Oh <ohseoyoung5@gmail.com>
2023-09-18 18:09:38 +0900

    lab4

--- testapp/App.js
+++ testapp/App.js
@@ -2,35 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, Image, TextInput, ScrollView, Button} from 'react-native';
 
-var head_st = {fontSize: 40, color:'blue', backgroundColor: 'orange', padding:10, margin:10};
-
 var text_st = {fontSize: 30, backgroundColor: 'lightgray', padding:10, margin:10};
 
 var input_st = {fontSize:30, borderWidth:1, padding: 10, margin: 10};
 
 export default function App() {
 
-  const [hello, sethello] = useState('Hello');
-
-  const [ name, setName ] = useState('');
-
-  function action() {
-    sethello('Nice to meet you')
-  }  
-
+  const [ greet, setGreet] = useState('Hi');
+  const [ name, setName ] = useState('Name');
 
   return (
     <ScrollView contentContainerStyle={{paddingTop: 30}}>
-      <Text style={text_st}>{hello}</Text>
-      <Text style={text_st}>{hello}</Text>
-      <Image style={{width:200, height:200}} source={require('./cat-icon.png')} />
-      <View style={{margin:10, flexDirection: "row"}}>
-        <Button title="Press Me" onPress={ function() { sethello('Hello, Ewha') }} />
-        <View style={{width:10}}></View>
-        <Button title="Touch Me" onPress={ action } />
-      </View>
+      <Text style={text_st}>{greet}, {name}</Text>
+      <TextInput style={input_st} onChangeText={ setName } />
 
-      <TextInput style={input_st} onChangeText={ function(t) { setName }} />
+      <View style={{margin:10, flexDirection: "row-reverse"}}>
+        <Button title="NICE" onPress={ function() { setGreet('Nice to meet you') }} />
+        <View style={{width:10}}></View>
+        <Button title="Hello" onPress={ function() { setGreet('Hello') }} />
+      </View>
       
     </ScrollView>
   );
