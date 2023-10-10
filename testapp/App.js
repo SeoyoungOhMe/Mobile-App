1c932fb7831e627566c6fdfe4bbb968ed69e6cce
Seoyoung Oh <ohseoyoung5@gmail.com>
2023-10-10 20:16:19 +0900

    lab1-1

--- testapp/App.js
+++ testapp/App.js
@@ -3,105 +3,39 @@ import React, {useState} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View, Image, TextInput, ScrollView, Button} from 'react-native';
+import { Audio } from 'expo-av';
 
-var text_st = {fontSize: 20, margin:10};
-var text2_st = {fontSize: 30, backgroundColor: 'lightgray', padding:10, margin:10};
-var text3_st = {fontSize: 30, padding:10, margin:10};
-var input_st = {fontSize: 20, borderWidth:1, padding:10, margin:10};
+async function play00(){
+  console.log('Loading Sound');
+  var s = await Audio.Sound.createAsync(require('./assets/note00.m4a'));
 
-function HelloScreen() {
-
-  const [ greet, setGreet ] = useState('Hi');
-  const [ name, setName ] = useState('Name');
-
-  return (
-    <View>
-      <Text style={text_st}>{greet}, {name}</Text>
-      <TextInput style={input_st} onChangeText={ setName } />
-
-      <View style={{margin:10, flexDirection:"row-reverse"}}>
-        <Button title="Nice"
-                onPress={ function() { setGreet('Nice to meet you')}} />
-        <View style={{width:10}}></View>
-        <Button title="Hello"
-                onPress={ function() { setGreet('Hello')}} />
-      </View>
-    </View>
-  );
+  console.log('Playing Sound');
+  s.sound.playAsync();
 }
 
-function AboutScreen() {
-  return (
-    <View>
-      <Text style={text_st}>This is about the app</Text>
-      <Image style={{width:200, height:200}} source={require('./cat-icon.png')} />
-    </View>
-  )
-};
-
-function MultScreen() {
-
-  const [ val, setval ] = useState(0);
-  const [A, setA] = useState(0);
-  const [B, setB] = useState(0);
-
-  return (
-    <ScrollView contentContainerStyle={{paddingTop: 30}}>
-      <View style={{ flexDirection: "row" }}>
-        <Text style={text2_st}>{A}</Text>
-        <Text style={text3_st}>X</Text>
-        <Text style={text2_st}>{B}</Text>
-        <Text style={text3_st}>=</Text>
-        <Text style={text2_st}> { Number(A) * Number(B) } </Text>
-      </View>
-      <View style={{margin:10, flexDirection:"row"}}>
-        <Button title="   +   "
-                onPress={ function() { setA(A+1) } } />
-        <View style={{width:70}}></View>
-        <Button title="   +   "
-                onPress={ function() { setB(B+1) } } />
-      </View>
-
-      <View style={{margin:10, flexDirection:"row"}}>
-        <Button title="    -   "
-                onPress={ function() { setA(A-1) } } />   
-        <View style={{width:70}}></View>     
-        <Button title="    -   "
-                onPress={ function() { setB(B-1) } } />
-      </View>
-
-    </ScrollView>
-  );
+async function play02(){
+  var s = await Audio.Sound.createAsync(require('./assets/note02.m4a'));
+  s.sound.playAsync();
 }
 
-function HomeScreen({ navigation }) {
-  return (
-    <View>
-      <Text style={text_st}>Home Screen</Text>
-      <Button title="About"
-        onPress={ function() { navigation.navigate('About') } } />
-      <View style={{height:10}} />
-      <Button title="Hello"
-        onPress={ function() { navigation.navigate('Hello') } } />
-      <View style={{height:10}} />
-      <Button title="구구단"
-        onPress={ function() { navigation.navigate('구구단') } } />
-    </View>
-  );
+async function play04(){
+  var s = await Audio.Sound.createAsync(require('./assets/note04.m4a'));
+  s.sound.playAsync();
 }
 
-const Stack = createStackNavigator();
+async function play05(){
+  var s = await Audio.Sound.createAsync(require('./assets/note05.m4a'));
+  s.sound.playAsync();
+}
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="About" component={AboutScreen} />
-        <Stack.Screen name="Hello" component={HelloScreen} />
-        <Stack.Screen name="구구단" component={MultScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={ {paddingTop:50} } >
+      <Button title="Play Sound" onPress={ play00 } />
+      <Button title="Play Sound" onPress={ play02 } />
+      <Button title="Play Sound" onPress={ play04 } />
+      <Button title="Play Sound" onPress={ play05 } />
+    </View>
   );
 }
 
