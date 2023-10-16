6eb86f27fadff6b498e08810e62e8742667608e6
Seoyoung Oh <ohseoyoung5@gmail.com>
2023-10-16 18:03:55 +0900

    lab 8-6

--- testapp/App.js
+++ testapp/App.js
@@ -24,7 +24,17 @@ export default function App() {
   var L = [ ];
 
   for(var i = 0; i < N.length; i++){
-    var a = <Text style={{fontSize:20}}>{N[i]} : {P[i]}</Text>
+    //var a = <Text style={{fontSize:20}}>{N[i]} : {P[i]}</Text>
+
+    var n = <Text style={{fontSize:20}}>{N[i]}</Text>
+     var p = <Text style={{fontSize:20}}>{P[i]}</Text>
+
+     var a = <View style={{flexDirection: "row"}}>
+       <View style={{width: "50%", borderWidth:1, padding:3}}>{n}</View>
+       <View style={{width:5}}></View>
+       <View style={{width: "48%", borderWidth:1, padding:3}}>{p}</View>
+     </View>
+     
     L.push(a);
   }
 
