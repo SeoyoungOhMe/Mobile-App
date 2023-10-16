580bb23f7b55e34958a0d4d35b031071e3b3c27b
Seoyoung Oh <ohseoyoung5@gmail.com>
2023-10-16 18:01:44 +0900

    lab 8-5

--- testapp/App.js
+++ testapp/App.js
@@ -5,30 +5,36 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View, Image, TextInput, ScrollView, Button, ImageBackground} from 'react-native';
 import { Audio } from 'expo-av';
 
+var input_st = {fontSize:20, borderWidth:1, flex:1, padding:5, margin:5};
+
 var N = ['Ewha', 'June', 'Jane', 'Nick'];
 var P = [1234, 3347, 1111, 1212];
 
 export default function App() {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [refresh, setRefresh] = useState(0);
+
+  function add_item() {
+    N.push(name);
+    P.push(phone);
+    setRefresh(refresh+1);
+  }
   
   var L = [ ];
 
   for(var i = 0; i < N.length; i++){
-//    var a = <Text style={{fontSize:20}}>{N[i]} : {P[i]}</Text>
-
-    var n = <Text style={{fontSize:20}}>{N[i]}</Text>
-    var p = <Text style={{fontSize:20}}>{P[i]}</Text>
-
-    var a = <View style={{flexDirection: "row"}}>
-      <View style={{width: "50%", borderWidth:1, padding:3}}>{n}</View>
-      <View style={{width:5}}></View>
-      <View style={{width: "48%", borderWidth:1, padding:3}}>{p}</View>
-    </View>
-
+    var a = <Text style={{fontSize:20}}>{N[i]} : {P[i]}</Text>
     L.push(a);
   }
 
   return <View style={{marginTop:30}}>
     <Text style={{fontSize:40}}>Phone Book</Text>
+    <View style={{margin:10, flexDirection:"row"}}>
+      <TextInput style={input_st} onChangeText={setName} />
+      <TextInput style={input_st} onChangeText={setPhone}/>
+      <Button title=" Add " onPress={add_item}  />
+    </View>
     {L}
   </View>
 }
