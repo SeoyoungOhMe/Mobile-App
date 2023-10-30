5cc7e56c40ded3990751d9a5ba46f4dd54194672
Seoyoung Oh <ohseoyoung5@gmail.com>
2023-10-30 19:31:55 +0900

    lab 11-5-3

--- testapp/App.js
+++ testapp/App.js
@@ -17,15 +17,16 @@ export default function App() {
 
   return (
    <View style={{flex:1, marginTop:40, alignItems:'center'}}>
-
     <BoxGroup>
-      <Text style={styles.text}>ABCD</Text>
-      <Button title="OK" />
-    </BoxGroup>
+      <BoxGroup>
+        <Text style={styles.text}>ABCD</Text>
+        <Button title="OK" />
+      </BoxGroup>
 
-    <BoxGroup>
-      <Text style={styles.text}>123</Text>
-      <Button title="OK" />
+      <BoxGroup>
+        <Text style={styles.text}>123</Text>
+        <Button title="OK" />
+      </BoxGroup>
     </BoxGroup>
 
    </View>
