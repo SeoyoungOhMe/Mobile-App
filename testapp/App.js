5dbca89c55f4b8be49e1d64b073eb3df16c32043
Seoyoung Oh <ohseoyoung5@gmail.com>
2023-11-13 16:53:28 +0900

    lab 14-2

--- testapp/App.js
+++ testapp/App.js
@@ -6,20 +6,35 @@ import { StyleSheet, Text, View, Image, TextInput, ScrollView, Button, ImageBack
 import { Audio } from 'expo-av';
 
 var url = "https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=af40fa5f16277c5903555d27da95ccdb"
+var st_text = { fontSize: 20 };
 
 export default function App() {
-  const [data, setData] = useState([]);
+  const [city, setCity] = useState('');
+  const [country, setCountry] = useState('');
+  const [temp, setTemp] = useState('');
+  const [weather, setWeather] = useState('');
+  const [wind, setWind] = useState('');
 
   useEffect(function() {
     fetch(url)
       .then(function (response) {return response.json(); })
-      .then(function (json) {console.log(json); setData(json); })
-      .then(function (error) {console.error(error);})
+      .then(function (json) {
+        console.log(json); 
+        setCity(json.name); 
+        setCountry(json.sys.country);
+        setTemp(json.main.temp);
+        setWeather(json.weather[0].description);
+        setWind(json.wind.speed); } )
+      .catch(function (error) { console.error(error); })
   }, []);
 
   return (
-    <View style={{flex:1, marginTop: 30, padding: 20 }}>
-      <Text>{JSON.stringify(data)}  </Text>
+    <View style={{ flex:1, marginTop: 30, padding: 20 }}>
+      <Text style={st_text}>City: {city}</Text>
+      <Text style={st_text}>Country: {country}</Text>
+      <Text style={st_text}>Temp: {temp}</Text>
+      <Text style={st_text}>Weather: {weather}</Text>
+      <Text style={st_text}>Wind: {wind}</Text>
     </View>
   );
 }
