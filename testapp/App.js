515ce947c45d8cc758ca1bf0a0fdd0e5da179a82
Seoyoung Oh <ohseoyoung5@gmail.com>
2023-11-13 16:47:09 +0900

    lab14-1a

--- testapp/App.js
+++ testapp/App.js
@@ -5,11 +5,13 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View, Image, TextInput, ScrollView, Button, ImageBackground, TouchableOpacity, TouchableHighlight, TouchableNativeFeedback } from 'react-native';
 import { Audio } from 'expo-av';
 
+var url = "https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=af40fa5f16277c5903555d27da95ccdb"
+
 export default function App() {
   const [data, setData] = useState([]);
 
   useEffect(function() {
-    fetch('https://reactnative.dev/movies.json')
+    fetch(url)
       .then(function (response) {return response.json(); })
       .then(function (json) {console.log(json); setData(json); })
       .then(function (error) {console.error(error);})
