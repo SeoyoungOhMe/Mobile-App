688430520d897d20768d58eabd4528a00078f839
Seoyoung Oh <ohseoyoung5@gmail.com>
2023-11-14 09:45:02 +0900

    lab14-4-3

--- testapp/App.js
+++ testapp/App.js
@@ -10,8 +10,7 @@ var st_text = { fontSize: 20 };
 
 
 export default function App() {
-//  const [city, setCity] = useState('');
-//  const [country, setCountry] = useState('');
+
   const [temp0, setTemp0] = useState('');
   const [weather0, setWeather0] = useState('');
   const [temp1, setTemp1] = useState('');
@@ -23,21 +22,18 @@ export default function App() {
   const [temp4, setTemp4] = useState('');
   const [weather4, setWeather4] = useState('');
 
-  const [sys0, setSys0] = useState('');
-  const [sys1, setSys1] = useState('');
-  const [sys2, setSys2] = useState('');
-  const [sys3, setSys3] = useState('');
-  const [sys4, setSys4] = useState('');
-//  const [wind, setWind] = useState('');
-//  const [icon, setIcon] = useState('');
+  const [icon0, setIcon0] = useState('');
+  const [icon1, setIcon1] = useState('');
+  const [icon2, setIcon2] = useState('');
+  const [icon3, setIcon3] = useState('');
+  const [icon4, setIcon4] = useState('');
 
   useEffect(function() {
     fetch(url)
       .then(function (response) {return response.json(); })
       .then(function (json) {
         console.log(json); 
-//        setCity(json.name); 
-//        setCountry(json.sys.country);
+
         setTemp0(json.list[0].main.temp);
         setWeather0(json.list[0].weather[0].description);
         setTemp1(json.list[1].main.temp);
@@ -49,24 +45,23 @@ export default function App() {
         setTemp4(json.list[4].main.temp);
         setWeather4(json.list[4].weather[0].description);
 
-        setSys0(json.list[0].dt_txt.substring(5, 16));
-        setSys1(json.list[1].dt_txt.substring(5, 16));
-        setSys2(json.list[2].dt_txt.substring(5, 16));
-        setSys3(json.list[3].dt_txt.substring(5, 16));
-        setSys4(json.list[4].dt_txt.substring(5, 16));
-//        setWind(json.wind.speed); 
-//        setIcon("http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
+        setIcon0("http://openweathermap.org/img/w/" + json.list[0].weather[0].icon + ".png");
+        setIcon1("http://openweathermap.org/img/w/" + json.list[1].weather[0].icon + ".png");
+        setIcon2("http://openweathermap.org/img/w/" + json.list[2].weather[0].icon + ".png");
+        setIcon3("http://openweathermap.org/img/w/" + json.list[3].weather[0].icon + ".png");
+        setIcon4("http://openweathermap.org/img/w/" + json.list[4].weather[0].icon + ".png");
+      
       } )
       .catch(function (error) { console.error(error); })
   }, []);
 
   return (
     <View style={{ flex:1, marginTop: 30, padding: 20 }}>
-      <Text style={st_text}>{`${sys0}\t${temp0}\t${weather0}`}</Text>
-      <Text style={st_text}>{`${sys1}\t${temp1}\t${weather1}`}</Text>
-      <Text style={st_text}>{`${sys2}\t${temp2}\t${weather2}`}</Text>
-      <Text style={st_text}>{`${sys3}\t${temp3}\t${weather3}`}</Text>
-      <Text style={st_text}>{`${sys4}\t${temp4}\t${weather4}`}</Text>
+      <Text style={st_text}>{`${icon0}\t${temp0}\t${weather0}`}</Text>
+      <Text style={st_text}>{`${icon1}\t${temp1}\t${weather1}`}</Text>
+      <Text style={st_text}>{`${icon2}\t${temp2}\t${weather2}`}</Text>
+      <Text style={st_text}>{`${icon3}\t${temp3}\t${weather3}`}</Text>
+      <Text style={st_text}>{`${icon4}\t${temp4}\t${weather4}`}</Text>
     </View>
   );
 }
