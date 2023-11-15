e71d5b89e8ac944cb04786456349111655d0cfe9
Seoyoung Oh <ohseoyoung5@gmail.com>
2023-11-15 21:30:54 +0900

    lab2-3a

--- testapp/App.js
+++ testapp/App.js
@@ -5,79 +5,25 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View, Image, TextInput, ScrollView, Button, ImageBackground, TouchableOpacity, TouchableHighlight, TouchableNativeFeedback } from 'react-native';
 import { Audio } from 'expo-av';
 
-var url = "http://api.openweathermap.org/data/2.5/forecast?q=Seoul&units=metric&cnt=5&appid=6af8dad4784c634d3674f60110f2a015"
-var st_text = { fontSize: 20 };
-
-
 export default function App() {
 
-  const [temp0, setTemp0] = useState('');
-  const [weather0, setWeather0] = useState('');
-  const [temp1, setTemp1] = useState('');
-  const [weather1, setWeather1] = useState('');
-  const [temp2, setTemp2] = useState('');
-  const [weather2, setWeather2] = useState('');
-  const [temp3, setTemp3] = useState('');
-  const [weather3, setWeather3] = useState('');
-  const [temp4, setTemp4] = useState('');
-  const [weather4, setWeather4] = useState('');
-
-  const [icon0, setIcon0] = useState('');
-  const [icon1, setIcon1] = useState('');
-  const [icon2, setIcon2] = useState('');
-  const [icon3, setIcon3] = useState('');
-  const [icon4, setIcon4] = useState('');
-
-  useEffect(function() {
-    fetch(url)
-      .then(function (response) {return response.json(); })
-      .then(function (json) {
-        console.log(json); 
-
-        setTemp0(json.list[0].main.temp);
-        setWeather0(json.list[0].weather[0].description);
-        setTemp1(json.list[1].main.temp);
-        setWeather1(json.list[1].weather[0].description);
-        setTemp2(json.list[2].main.temp);
-        setWeather2(json.list[2].weather[0].description);
-        setTemp3(json.list[3].main.temp);
-        setWeather3(json.list[3].weather[0].description);
-        setTemp4(json.list[4].main.temp);
-        setWeather4(json.list[4].weather[0].description);
-
-        setIcon0("http://openweathermap.org/img/w/" + json.list[0].weather[0].icon + ".png");
-        setIcon1("http://openweathermap.org/img/w/" + json.list[1].weather[0].icon + ".png");
-        setIcon2("http://openweathermap.org/img/w/" + json.list[2].weather[0].icon + ".png");
-        setIcon3("http://openweathermap.org/img/w/" + json.list[3].weather[0].icon + ".png");
-        setIcon4("http://openweathermap.org/img/w/" + json.list[4].weather[0].icon + ".png");
-      
-      } )
-      .catch(function (error) { console.error(error); })
-  }, []);
-
   return (
-    <View style={{ flex:1, marginTop: 30, padding: 20 }}>
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Image style={{ width: 50, height: 50 }} source={{ uri: icon0 }} />
-        <Text style={st_text}>{`\t${temp0}\t${weather0}`}</Text>
-      </View> 
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Image style={{ width: 50, height: 50 }} source={{ uri: icon1 }} />
-        <Text style={st_text}>{`\t${temp1}\t${weather1}`}</Text>
-      </View> 
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Image style={{ width: 50, height: 50 }} source={{ uri: icon2 }} />
-        <Text style={st_text}>{`\t${temp2}\t${weather2}`}</Text>
-      </View> 
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Image style={{ width: 50, height: 50 }} source={{ uri: icon3 }} />
-        <Text style={st_text}>{`\t${temp3}\t${weather3}`}</Text>
-      </View> 
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Image style={{ width: 50, height: 50 }} source={{ uri: icon4 }} />
-        <Text style={st_text}>{`\t${temp4}\t${weather4}`}</Text>
-      </View> 
-    </View>
+    <ScrollView 
+      contentContainerStyle={{paddingTop:50, borderColor:'red', borderWidth:10}}
+      style={{ borderColor:'blue', borderWidth:10}}>
+      <Text style={{fontSize:40, color:'blue'}}>Hello Ewha</Text>
+      <Text>Nice to meet you</Text>
+      <Text style={{fontSize:20}}>Nice to meet you</Text>
+      <Text style={{fontSize:30}}>Nice to meet you</Text>
+      <Image style={{width:200, height:200}} source={require('./cat-icon.png')} />
+      <Image style={{width:200, height:200}} source={{uri: 'https://reactnative.dev/docs/assets/p_cat2.png'}} />
+      <Text style={{fontSize:20}}>Nice to meet you</Text>
+      <Text style={{fontSize:30}}>Nice to meet you</Text>
+      <Image style={{width:200, height:200}} source={require('./cat-icon.png')} />
+      <Text style={{fontSize:20}}>Nice to meet you</Text>
+      <Text style={{fontSize:30}}>Nice to meet you</Text>
+      <Image style={{width:200, height:200}} source={require('./cat-icon.png')} />
+    </ScrollView>
   );
 }
 
