ca835866f3b93ce94524b9bf4081f3711a21c89e
Seoyoung Oh <ohseoyoung5@gmail.com>
2023-11-27 16:58:35 +0900

    lab15-1

--- testapp/App.js
+++ testapp/App.js
@@ -1,44 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet, Text, View, Image, TextInput, ScrollView, Button, ImageBackground} from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, ScrollView, Button, ImageBackground, TouchableOpacity, TouchableHighlight, TouchableNativeFeedback } from 'react-native';
 import { Audio } from 'expo-av';
-
-var input_st = {fontSize:20, borderWidth:1, flex:1, padding:5, margin:5};
-
-var pbook = [{name:'Ewha', phone:1234}, {name:'June', phone:3347},
-            {name:'Jane', phone:1111},{name:'Nick', phone:1212}]
+import CalendarPicker from 'react-native-calendar-picker';
 
 export default function App() {
-  
-    const [name, setName] = useState('');
-    const [phone, setPhone] = useState('');
-    const [refresh, setRefresh] = useState('');
 
-    function add_item(){
-      pbook.push({ name: name, phone:phone });
-      setRefresh(refresh+1);
-    }
+  const [date, setDate] = useState('')
+  const [date1, setDate1] = useState('')
 
-    var L = [];
+  function onDateChange(d) {
+    console.log(d);
+    console.log(d.toString());
+    console.log(d.format('YYYYMMDD'));
+    console.log(d.format('MMMM DD, YYYY'));
 
-    for (var i=0; i<pbook.length; i++){
-      var a = <Text style={{ fontSize: 20 }}>{pbook[i].name} : {pbook[i].phone}</Text>
-      L.push(a);
-    }
+    setDate(d.format('YYYYMMDD'));
+    setDate1(d.format('MMMM DD, YYYY'));
+  }
 
-    return <View style={{marginTop:50}}>
-      <Text style={{fontSize:40}}>Phone Book</Text>
-      <View style={{margin:10, flexDirection: "row"}}>
-        <TextInput style={input_st} onChangeText={setName} />
-        <TextInput style={input_st} onChangeText={setPhone} />
-        <Button title="  ADD  " onPress={add_item} />
+  return (
+    <View style={styles.container}>
+      <CalendarPicker onDateChange={onDateChange}/>
+      <View style={styles.box}>
+        <Text>Date: { date }</Text>
+        <Text style={styles.text}>{ date1 }</Text>
       </View>
-      {L}
     </View>
-    
-  
+  );
 }
 
 const styles = StyleSheet.create({
@@ -48,4 +39,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   }
-});
+  },
+);
