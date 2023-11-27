d80ebf3f9e2f534f0011becef5c53fa2bec4da62
Seoyoung Oh <ohseoyoung5@gmail.com>
2023-11-27 17:17:50 +0900

    lab15-3

--- testapp/App.js
+++ testapp/App.js
@@ -5,6 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View, Image, TextInput, ScrollView, Button, ImageBackground, TouchableOpacity, TouchableHighlight, TouchableNativeFeedback } from 'react-native';
 import { Audio } from 'expo-av';
 import CalendarPicker from 'react-native-calendar-picker';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
 
@@ -13,15 +14,34 @@ export default function App() {
   const [expense, setExpense] = useState('')
   const [memo, setMemo] = useState('')
  
-  function onDateChange(d) {
+  async function date_change(d) {
     console.log(d.format('YYYYMMDD'));
     setDate(d.format('YYYYMMDD'));
     setDate1(d.format('MMMM DD, YYYY'));
+
+    var key = d.format('YYYYMMDD');
+    var value = await AsyncStorage.getItem(key);
+    var value_m = await AsyncStorage.getItem(key + 'm');
+
+    console.log(value);
+
+    if (value !== null) {
+      setExpense(value)
+      setMemo(value_m)
+    } else {
+      setExpense('')
+      setMemo('')
+    }
+  }
+
+  async function save_memo(){
+    await AsyncStorage.setItem(date, expense)
+    await AsyncStorage.setItem(date+'m', memo)
   }
 
   return (
     <View style={styles.container}>
-      <CalendarPicker onDateChange={onDateChange}/>
+      <CalendarPicker onDateChange={date_change}/>
       <View style={styles.box}>
         <Text style={styles.datetext}>{ date1 }</Text>
         <View style={styles.row}>
