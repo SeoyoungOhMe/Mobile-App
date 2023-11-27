5ddc64fa83bdab425110398c00973f0c2731be83
Seoyoung Oh <ohseoyoung5@gmail.com>
2023-11-27 17:27:53 +0900

    lab15-4

--- testapp/App.js
+++ testapp/App.js
@@ -7,53 +7,78 @@ import { Audio } from 'expo-av';
 import CalendarPicker from 'react-native-calendar-picker';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function App() {
+const Stack = createStackNavigator();
 
-  const [date, setDate] = useState('')
-  const [date1, setDate1] = useState('')
-  const [expense, setExpense] = useState('')
-  const [memo, setMemo] = useState('')
- 
+export default function App() {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Memo" component={MemoScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen({ navigation }){
+
   async function date_change(d) {
     console.log(d.format('YYYYMMDD'));
-    setDate(d.format('YYYYMMDD'));
-    setDate1(d.format('MMMM DD, YYYY'));
+    var date = d.format('YYYYMMDD');
 
-    var key = d.format('YYYYMMDD');
-    var value = await AsyncStorage.getItem(key);
-    var value_m = await AsyncStorage.getItem(key + 'm');
+    var val_expense = '';
+    var val_memo = '';
+
+    var value = await AsyncStorage.getItem(date);
+    var value_m = await AsyncStorage.getItem(date + 'm');
 
     console.log(value);
 
     if (value !== null) {
-      setExpense(value)
-      setMemo(value_m)
-    } else {
-      setExpense('')
-      setMemo('')
+      val_expense = value;
+      val_memo = value_m;
     }
-  }
 
-  async function save_memo(){
-    await AsyncStorage.setItem(date, expense)
-    await AsyncStorage.setItem(date+'m', memo)
+    navigation.navigate('Memo',
+    {date: d, expense: val_expense, memo: val_memo });
   }
 
   return (
     <View style={styles.container}>
       <CalendarPicker onDateChange={date_change}/>
-      <View style={styles.box}>
-        <Text style={styles.datetext}>{ date1 }</Text>
-        <View style={styles.row}>
-          <Text style={styles.text}>Expense</Text>
-          <TextInput style={styles.expense} keyboardType="numeric" onChangeText={setExpense} />
-          <Button title="Save" />
-        </View>
-        <TextInput style={styles.memo} placeholder='Memo' onChangeText={setMemo} />
-      </View>
     </View>
   );
 }
+
+function MemoScreen({ route, navigation }){
+
+  var d = route.params.date;
+
+  const [date, setDate] = useState('YYYYMMDD');
+  const [date1, setDate1] = useState('MMMM DD, YYYY');
+  const [expense, setExpense] = useState(route.params.expense);
+  const [memo, setMemo] = useState(route.params.memo);
+
+  async function save_memo(){
+      await AsyncStorage.setItem(date, expense)
+      await AsyncStorage.setItem(date+'m', memo)
+    }
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.box}>
+          <Text style={styles.datetext}>{ date1 }</Text>
+          <View style={styles.row}>
+            <Text style={styles.text}>Expense</Text>
+            <TextInput style={styles.expense} keyboardType="numeric" onChangeText={setExpense} />
+            <Button title="Save" />
+          </View>
+          <TextInput style={styles.memo} placeholder='Memo' onChangeText={setMemo} />
+        </View>
+      </View>
+    );
+}
+
 
 const styles = StyleSheet.create({
   container: {
